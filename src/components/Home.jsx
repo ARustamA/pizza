e60bd36dbc579a9fdea6a368c591d2f45842e28b
“ARustamA” <rakhmetyanov@gmail.com>
2022-07-20 11:34:23 +0500

    17

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,21 +7,24 @@ import {useNavigate} from 'react-router-dom'
 import Skeleton from './Skeleton'
 import PizzaCart from './PizzaCard'
 import Categories from './Categories'
-import Sort, { sortArr } from './Sort'
+import Sort from './Sort'
 import Pagination from './Pagination'
-import { setCategoriesId, setFilters } from '../redux/slices/filterSlice'
+import { setCategoriesId } from '../redux/slices/filterSlice'
 import { AppContext } from '../App'
 
 import '../scss/app.scss' 
+import { setItems } from '../redux/slices/pizzaSlice'
 
 
 function Home() {
    const navigate = useNavigate()
    const {categoriesId, sortIndex, pageCount } = useSelector((state) => state.filterSlice)
+   const items  = useSelector((state) => state.pizzaSlice.items)
+
    const dispatch =  useDispatch()
    const onClickCategories = (id) => { dispatch(setCategoriesId(id)) }
 
-   const [pizzaItems, setPizzaItems] = React.useState([])
+
    const [isLoading, setIsLoading] = React.useState(true)
    const { searchValue } = React.useContext(AppContext)
 
@@ -42,26 +45,28 @@ function Home() {
    // },[])
 
 //получение пицц
-   
-React.useEffect(() => {
-      async function generalData() {
-         try {
-            setIsLoading(true)
-            const order = sortIndex.sortProperty.includes('-') ? 'asc' : 'desc'
-            const sorty = sortIndex.sortProperty.replace('-', '')
-            const category = categoriesId > 0 ? `category=${categoriesId}` : ''
-            const search = searchValue ? `&search=${searchValue}` : ''
-            const { data } = await axios.get(`https://62cbce7d8042b16aa7c2c215.mockapi.io/items?page=${pageCount}&limit=4&${category}&sortBy=${sorty}&order=${order}${search}`)
+async function generalData() {
+   try {
+      setIsLoading(true)
+      const order = sortIndex.sortProperty.includes('-') ? 'asc' : 'desc'
+      const sorty = sortIndex.sortProperty.replace('-', '')
+      const category = categoriesId > 0 ? `category=${categoriesId}` : ''
+      const search = searchValue ? `&search=${searchValue}` : ''
+      const { data } = await axios.get(`https://62cbce7d8042b16aa7c2c215.mockapi.io/items?page=${pageCount}&limit=4&${category}&sortBy=${sorty}&order=${order}${search}`)
 
-            setPizzaItems(data)
-            setIsLoading(false)
-         } catch (error) {
-            console.log(error)
-            alert('Не удалось выгрузить данные с сервера')
-         }
-      }
+      dispatch(setItems(data))
+      
+   } catch (error) {
+      console.log(error)
+      alert('Не удалось выгрузить данные с сервера')
+   } finally{
+      setIsLoading(false)
+   }
+}
+React.useEffect(() => {
+   
       generalData()
-      // window.scrollTo(0, 0)
+      //  window.scrollTo(0, 0)
    }, [categoriesId, sortIndex.sortProperty, searchValue, pageCount])
 
 
@@ -76,9 +81,8 @@ React.useEffect(() => {
    }, [categoriesId, sortIndex.sortProperty, pageCount])
 
 
-   
-   const pizzasAr = pizzaItems.map((obj) => (<PizzaCart key={obj.id} {...obj} />))
-   const skeletons = [... new Array(pageCount)].map((_, i) => (<Skeleton key={i} />))
+   const pizzasAr = items.map((obj) => (<PizzaCart key={obj.id} {...obj} />))
+   const skeletons = [...new Array(pageCount)].map((_, i) => (<Skeleton key={i} />))
 
    return (
       <>
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import filterSlice from '../redux/slices/filterSlice'
 import cartSlice from '../redux/slices/cartSlice'
+import pizzaSlice from '../redux/slices/pizzaSlice'
 
 export const store = configureStore({
    reducer: {
       filterSlice,
-      cartSlice
+      cartSlice,
+      pizzaSlice,
    },
 })
